13b1553c8e21d1b57bd595e749f6eb9aa67974a0
DESKTOP-3F0BJ1R\gabes <gab.espinoza@gmail.com>
2018-05-22 01:28:02 -0400

    Se implementa sonido usando plugin nuevo

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'theme.dart';
-import 'songs.dart';
-import 'package:fluttery_audio/fluttery_audio.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayer/audioplayer.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new Botonera());
 
-class MyApp extends StatelessWidget{
+class Botonera extends StatefulWidget{
+   @override
+  _BotoneraState createState() => new _BotoneraState();
+}
+
+enum PlayerState { stopped, playing, paused }
+
+class _BotoneraState extends State<Botonera>{
   @override
   Widget build(BuildContext context){
     return new MaterialApp(
@@ -16,13 +23,11 @@ class MyApp extends StatelessWidget{
         children: <Widget>[
           new RaisedButton(
             child: new Image(image: new NetworkImage("https://www.mariowiki.com/images/8/8b/MarioPartyStarRushStar.png")),
-            onPressed: ()=>{
-              
-            },
+            onPressed: ()=>_playAudio("star"),
             color: Colors.black,
           ),
           new RaisedButton(
-            child: new Text("Item 2"),
+            child: new Text("Item 1231"),
             onPressed: ()=>{},
           ),
           new RaisedButton(
@@ -45,86 +50,20 @@ class MyApp extends StatelessWidget{
       ),
     );
   }
-}
-
-
-
-
-
-
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<StatefulWidget> {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new Audio(
-      audioUrl: demoPlaylist.songs[0].audioUrl,
-      playbackState: PlaybackState.paused,
-      child: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('hola'),
-        ),
-        body: new Column(
-
-          children: <Widget>[
-
-            new Container(
-              child: new Center(
-                child: new Container(
-                  width: 125.0,
-                  height: 125.0,
-                ),
-              ),
-            ),
-            
-            new AudioComponent(
-              updateMe:[
-                WatchableAudioProperties.audioPlayerState,
-              ],
-              playerBuilder: (BuildContext context, AudioPlayer player, Widget child)
-              {
-                Function onPressed;
-                onPressed = player.play;
-                if(player.state == AudioPlayerState.playing)
-                {
-                  onPressed = player.pause;
-                } 
-                else if (player.state == AudioPlayerState.paused || player.state == AudioPlayerState.completed)
-                {
-                  onPressed = player.play;
-                }
-
-                return new RawMaterialButton(
-                  shape: new CircleBorder(),
-                  fillColor: Colors.blue,
-                  // splashColor: lightAccentColor,
-                  // highlightColor: lightAccentColor.withOpacity(0.5),
-                  elevation: 10.0,
-                  highlightElevation: 5.0,
-                  onPressed: onPressed,
-
-                  child: new Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: new Icon(
-                      Icons.play_arrow,
-                      color: Colors.black,
-                      size: 35.0,
-                    ),
-                  ),
-                );
-              }
-            )
-          ],
-        ),
-      ),
-    );
-    
+  
+  AudioPlayer apStar = new AudioPlayer();
+  PlayerState psStar = PlayerState.stopped;
+  Future<Null> _playAudio(String sound) async {
+    if(psStar == PlayerState.playing){
+      final result = apStar.pause();
+      if(result == 1)
+        psStar = PlayerState.paused;
+    }
+    else
+    {
+      final result = await apStar.play("https://api.soundcloud.com/tracks/434370309/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P");
+      if (result == 1) 
+        setState(() => psStar = PlayerState.playing);
+    }
   }
 }
-
-
